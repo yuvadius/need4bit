74c572780fa1fd8d40d7ad286f6e2dc28a4363c6
Yuval Glizerin <yuvalgliz@gmail.com>
2016-07-20 08:44:48 +0300

    We have Dynamites!

---
+++ Bombs Away/Assets/Scripts/DynamiteExplode.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DynamiteExplode : MonoBehaviour {
+    public float timeDestroy;
+	// Use this for initialization
+	void Start () {
+        Destroy(gameObject, timeDestroy);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Bombs Away/Assets/Scripts/PlayerController.cs
+++ Bombs Away/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,9 @@ using System.Collections;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D rb;
+    private float lastTime = 0;
+    //The time to wait untill another dynamite can be created
+    public float timeWait;
     public float speed;
 
     // Use this for initialization
@@ -23,5 +26,10 @@ public class PlayerController : MonoBehaviour
         float moveH = Input.GetAxis("Horizontal") * speed;
         float moveV = Input.GetAxis("Vertical") * speed;
         rb.AddForce(new Vector2(moveH, moveV));
+        if (Input.GetKeyDown("space") && (lastTime == 0 || Time.fixedTime - lastTime >= timeWait))
+        {
+            Instantiate(Resources.Load("Dynamite"), gameObject.transform.position, gameObject.transform.rotation);
+            lastTime = Time.fixedTime;
+        }
     }
 }

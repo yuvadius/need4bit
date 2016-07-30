074f8709dd1b1f7a080afd29303ea440d58f5ce0
TinyTeaTree <iglizerin@gmail.com>
2016-07-31 00:07:21 +0300

    Snake Recording Player
    Ghost snake now performs the trail

---
+++ Snake/GlobeSnake3D/Assets/Scripts/Snake Scripts/SnakeRecorder.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+
+
+/// <summary>
+/// This will create a recording structure for a given snake
+/// </summary>
+public class SnakeRecorder : MonoBehaviour {
+
+	public RemoteRecordingData recording;
+
+	public Transform recordingPivot;
+
+	int frameCount = 0;
+
+	void Update()
+	{
+		frameCount++;
+		recording.data.pointsToRecord.Add(new RemotePoint(recordingPivot.position, recordingPivot.rotation, frameCount));
+	}
+
+	[ContextMenu("Record")]
+	public void Record()
+	{
+		BinaryFormatter bf = new BinaryFormatter();
+		FileStream fs = File.Create(Application.persistentDataPath + "/dataRecord.dat");
+
+		bf.Serialize(fs, recording.data);
+		fs.Close();
+		fs.Dispose();
+
+		Debug.Log("Path to file is: " + Application.persistentDataPath);
+	}
+
+}

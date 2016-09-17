ed94b08530c14e9398d43e8ea714e3a5d38c663f
Yuval Glizerin <yuvalgliz@gmail.com>
2016-09-17 04:09:20 +0300

    progress

---
+++ Sneak Pic/Sneak Pic/www/js/script.js
@@ -0,0 +1,78 @@
+var gallery;
+var items = [];
+var isGalleryUp;
+var pswpElement;
+var verticalCenter = '<div style="display: table;width: 100%;height: 100%;"><div style="display: table-cell;vertical-align: middle;">';
+
+$(document).ready(function() {
+    GetEvents();
+});
+
+function GetEvents() {
+  $.ajax({
+      url: "https://need4bit.com/SneakPic/Events/",
+      success: function(data){
+        $(".feed").empty();
+        aLinks = $(data).find("a");
+        aLinks.each(function(){
+            href = $(this).attr("href");
+            if(href.indexOf('/SneakPic/Events/') != -1) {
+              name = href.replace('/SneakPic/Events/','').slice(0,-1);
+              $(".feed").append("<div onclick=\"GetPictures(this)\" name=\""+decodeURI(name)+"\" class=\"event greyscale\" style=\"background:url('https://need4bit.com"+href+"banner.jpg');background-size:cover;\">"+verticalCenter+decodeURI(name)+"</div></div></div>");
+            }
+        });
+      }
+    });
+}
+
+function GetPictures(selector) {
+  items = [];
+  imgCounter = 0;
+  $.ajax({
+      url: "https://need4bit.com/SneakPic/Events/" + $(selector).attr("name") + "/images/",
+      success: function(data){
+        $(".feed").empty();
+        aLinks = $(data).find("a");
+        imgLength = aLinks.length;
+        aLinks.each(function(){
+            href = $(this).attr("href");
+            if(href.indexOf('.jpg') != -1) {
+              var img = new Image();
+              img.onload = function() {
+                $(".feed").append("<div onclick=\"clickImage(this)\" id=\""+imgCounter+"\" class=\"img\" style=\"background:url('"+img.src+"');background-size:cover;\"></div>");
+                imgCounter++;
+              }
+              item = {src: "https://need4bit.com"+href, w: 625, h: 417};
+              items.push(item);
+              console.log(items);
+              img.src = "https://need4bit.com"+href;
+            }
+        });
+      }
+    });
+}
+
+function clickImage() {
+  //gallery.close();
+  openGallery($(this).attr('id'));
+}
+
+function openGallery(x) {
+  isGalleryUp = true;
+  pswpElement = document.querySelectorAll('.pswp')[0];
+  
+  // define options (if needed)
+  var options = {
+      // history & focus options are disabled on CodePen        
+      history: false,
+      focus: false,
+
+      showAnimationDuration: 0,
+      hideAnimationDuration: 0
+      
+  };
+  
+  gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
+  gallery.init();
+  gallery.goTo(x);
+}

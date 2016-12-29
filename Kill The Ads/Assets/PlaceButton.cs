8ac7f10cc4bd2c410c6a908998272b22411709b7
Yuval Glizerin <yuvalgliz@gmail.com>
2016-12-29 15:35:58 +0200

    Created Ad

---
+++ Kill The Ads/Assets/PlaceButton.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaceButton : MonoBehaviour {
+    public GameObject ad;
+	// Use this for initialization
+	void Start () {
+        //transform.position = new Vector3(ad.position.x, ad.position.y);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.localPosition = new Vector3(0, 0);
+    }
+}
---
+++ Kill The Ads/Assets/SelfDestruct.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SelfDestruct : MonoBehaviour {
+    public void DestroyThisObject()
+    {
+        Destroy(gameObject);
+    }
+}

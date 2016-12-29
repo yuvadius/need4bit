62a84a402f929a522baf903d4900105afc3ea900
masterChiefer <yudagliz73@gmail.com>
2016-12-29 16:19:49 +0200

    added controller

---
+++ Kill The Ads/Assets/Scripts/Game_Controller.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Game_Controller : MonoBehaviour 
+{
+    private float curr_time;
+    public float pop_rate;
+    public GameObject pread;
+
+	// Use this for initialization
+	void Start () 
+    {
+        curr_time = 0f;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        curr_time += Time.deltaTime;
+
+        if (curr_time > pop_rate)
+        {
+            curr_time = 0f;
+            Vector3 pos = new Vector3(Random.Range(-2.4f, 2.4f), Random.Range(-1.1f, 1.1f), 0);
+            Quaternion rot = new Quaternion(0f,0f,0f, 0f);
+            Instantiate(pread, pos, rot);
+            
+        }
+	}
+}
